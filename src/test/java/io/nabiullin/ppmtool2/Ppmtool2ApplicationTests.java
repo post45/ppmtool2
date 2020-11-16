055d6f210d6c0f19c5af7d7baa6eacc05bfb99b7
post45 <bashkuresh92@yandex.ru>
2020-11-16 13:34:00 -0500

    first commit

---
+++ src/test/java/io/nabiullin/ppmtool2/Ppmtool2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nabiullin.ppmtool2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ppmtool2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
