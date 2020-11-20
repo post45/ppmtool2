21fe0cec43603e027897bd8cdbece6d324048869
post45 <bashkuresh92@yandex.ru>
2020-11-19 20:50:10 -0500

    getAllProjects commit

--- src/main/java/io/nabiullin/ppmtool2/services/ProjectService.java
+++ src/main/java/io/nabiullin/ppmtool2/services/ProjectService.java
@@ -20,6 +20,22 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
         }
 
-
     }
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
+        }
+
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProject(){
+        return projectRepository.findAll();
+    }
+
 }
