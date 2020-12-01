b402d625a39d1bf79e10783dcb10082f7ca61218
post45 <bashkuresh92@yandex.ru>
2020-12-01 11:50:13 -0500

    summary 57

--- src/main/java/io/nabiullin/ppmtool2/services/ProjectTaskService.java
+++ src/main/java/io/nabiullin/ppmtool2/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package io.nabiullin.ppmtool2.services;
 
 import io.nabiullin.ppmtool2.domain.Backlog;
+import io.nabiullin.ppmtool2.domain.Project;
 import io.nabiullin.ppmtool2.domain.ProjectTask;
+import io.nabiullin.ppmtool2.exceptions.ProjectNotFoundException;
 import io.nabiullin.ppmtool2.repositories.BacklogRepository;
+import io.nabiullin.ppmtool2.repositories.ProjectRepository;
 import io.nabiullin.ppmtool2.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,48 +15,97 @@ import java.util.List;
 @Service
 public class ProjectTaskService {
 
+
     @Autowired
     private BacklogRepository backlogRepository;
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
 
-        //Exceptions: Project not found
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 
-        //PTs to be added to a specific project, project != null, BL exists
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        //set the bl to pt
-        projectTask.setBacklog(backlog);
-        //we want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
-        Integer BacklogSequence = backlog.getPTSequence();
-        // Update the BL SEQUENCE
-        BacklogSequence++;
+        try {
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
+            Integer BacklogSequence = backlog.getPTSequence();
+            BacklogSequence++;
 
-        backlog.setPTSequence(BacklogSequence);
+            backlog.setPTSequence(BacklogSequence);
 
-        //Add Sequence to Project Task
-        projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        //INITIAL priority when priority null
 
-        //INITIAL status when status is null
-        if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
-            projectTask.setStatus("TO_DO");
+            if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+            }
+
+            if (projectTask.getPriority() == null) { //In the future we need projectTask.getPriority()== 0 to handle the form
+                projectTask.setPriority(3);
+            }
+
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project not Found");
         }
 
-        if(projectTask.getPriority()==null){ //In the future we need projectTask.getPriority()== 0 to handle the form
-            projectTask.setPriority(3);
+    }
+
+    public Iterable<ProjectTask> findBacklogById(String id) {
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if (project == null) {
+            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
         }
+
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+    }
+
+
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(backlog_id);
+        if (backlog == null) {
+            throw new ProjectNotFoundException("Project with ID: '" + backlog_id + "' does not exist");
+        }
+
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+
+        if (projectTask == null) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' not found");
+        }
+
+        if (!projectTask.getProjectIdentifier().equals(backlog_id)) {
+            throw new ProjectNotFoundException("Project Task '" + pt_id + "' does not exist in project: '" + backlog_id);
+        }
+
+
+        return projectTask;
+    }
+
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
+        ProjectTask projectTask =  findPTByProjectSequence(backlog_id, pt_id);
+
+        projectTask = updatedTask;
 
         return projectTaskRepository.save(projectTask);
     }
 
-    public Iterable<ProjectTask>findBacklogById(String id){
-        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
+        Backlog backlog = projectTask.getBacklog();
+        List<ProjectTask> pts = backlog.getProjectTasks();
+        pts.remove(projectTask);
+        backlogRepository.save(backlog);
+
+        projectTaskRepository.delete(projectTask);
     }
 }
 
--- src/main/java/io/nabiullin/ppmtool2/web/BacklogController.java
+++ src/main/java/io/nabiullin/ppmtool2/web/BacklogController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/backlog")
@@ -24,12 +23,10 @@ public class BacklogController {
     private MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("/{backlog_id}")
-    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
-                                            BindingResult result, @PathVariable String backlog_id){
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id) {
 
-
-        ResponseEntity<?> erroMap = mapValidationErrorService.MapValidationService(result);
-        if (erroMap != null) return erroMap;
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
 
         ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
 
@@ -38,9 +35,35 @@ public class BacklogController {
     }
 
     @GetMapping("/{backlog_id}")
-    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id){
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id) {
 
         return projectTaskService.findBacklogById(backlog_id);
 
+    }
+
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id) {
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
+
+
+    @PatchMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> updateProjectTask(@Valid @RequestBody ProjectTask projectTask, BindingResult result,
+                                               @PathVariable String backlog_id, @PathVariable String pt_id ){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask,backlog_id,pt_id);
+
+        return new ResponseEntity<ProjectTask>(updatedTask,HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        projectTaskService.deletePTByProjectSequence(backlog_id, pt_id);
+
+        return new ResponseEntity<String>("Project Task "+pt_id+" was deleted successfully", HttpStatus.OK);
     }
 }
